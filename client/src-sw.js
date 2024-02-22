7c99319bcb335f91b4cb39a136be7bd155350335
Katy Thompson <katimation@gmail.com>
2024-02-21 20:50:18 -0600

    updated src-sw.js and database.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,30 +1,40 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
+const { registerRoute } = require('workbox-routing');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
-precacheAndRoute(self.__WB_MANIFEST);
+// precacheAndRoute(self.__WB_MANIFEST);
 
-const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+// const pageCache = new CacheFirst({
+//   cacheName: 'page-cache',
+//   plugins: [
+//     new CacheableResponsePlugin({
+//       statuses: [0, 200],
+//     }),
+//     new ExpirationPlugin({
+//       maxAgeSeconds: 30 * 24 * 60 * 60,
+//     }),
+//   ],
+// });
+
+// warmStrategyCache({
+//   urls: ['/index.html', '/'],
+//   strategy: pageCache,
+// });
+
+// registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// // TODO: Implement asset caching
+registerRoute(({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+ new StaleWhileRevalidate({
+  cacheName: 'asset-cache',
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
   ],
-});
+})
+);
 
-warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
-});
-
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
-// TODO: Implement asset caching
-registerRoute();
